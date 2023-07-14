fae685a9d25aa4da5b9953d9a10432b1a5f5dc36
shaheedulaslam <shaheedulaslam061@gmail.com>
2023-07-14 15:44:16 +0530

    finished

--- src/pages/components/subcomponent/ReusableComponents.tsx
+++ src/pages/components/subcomponent/ReusableComponents.tsx
@@ -1,47 +1,47 @@
 import React from 'react'
 
 interface BodyButtonProps {
-    value: string;
-  }
+  value: string;
+}
 
 export function Explorebutton() {
-  return (   
-<button className='border-2 rounded-full mt-5 px-4 py-1 text-[#0744C3] border-[#0744C3]'>Explore</button>
+  return (
+    <button className='border-2 rounded-full mt-5 px-4 py-1 text-[#0744C3] border-[#0744C3]'>Explore</button>
   )
 }
 
 
-export const BodyButton: React.FC<BodyButtonProps> = ({ value }) => { 
-return(
+export const BodyButton: React.FC<BodyButtonProps> = ({ value }) => {
+  return (
     <button
-        type="button"
-        className="rounded-full px-12 py-2 text-base bg-blue-700 hover:bg-blue-800 hover:text-white text-white mt-2 duration-200">
-        {value}</button>
-)
-}
-
-export function InterviewsParagraph(){
-  return(
-<p className="text-sm text-left text-gray-500 font-light mt-2">
-  I'm Sayan, founder and CTO of Desygner . Over 15,000 B2B customers use Desygner's B2B platform
-  webrand.com for..
-</p>
+      type="button"
+      className="rounded-full px-12 py-2 text-base bg-blue-700 hover:bg-blue-800 hover:text-white text-white mt-2 duration-200">
+      {value}</button>
   )
 }
 
-export function ServiceParagraph(){
-  return(
-  <p className="m-0 max-w-[30ch] text-sm opacity-50 text-center">
-    Explore all the remote work visas available around the world.
-  </p>
+export function InterviewsParagraph() {
+  return (
+    <p className="text-sm text-left text-gray-500 font-light mt-2">
+      I'm Sayan, founder and CTO of Desygner . Over 15,000 B2B customers use Desygner's B2B platform
+      webrand.com for..
+    </p>
   )
 }
 
-export function StartUpVisaParagrapgh(){
-return(
-  <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-center`}>
-  Explore all the startup visas available around the world.
-</p>
-)
+export function ServiceParagraph() {
+  return (
+    <p className="m-0 max-w-[30ch] text-sm opacity-50 text-center">
+      Explore all the remote work visas available around the world.
+    </p>
+  )
+}
+
+export function StartUpVisaParagrapgh() {
+  return (
+    <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-center`}>
+      Explore all the startup visas available around the world.
+    </p>
+  )
 }
 
