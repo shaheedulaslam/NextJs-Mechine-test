0b164730f3aab13b6c681aabc00b3d141cea833c
shaheedulaslam <shaheedulaslam061@gmail.com>
2023-07-14 14:32:21 +0530

    title added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <title>Oye StartUp's</title>
       <Head />
       <body>
         <Main />
